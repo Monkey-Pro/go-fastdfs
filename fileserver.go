a49a0fa5c00f8be2994bb5d315d6159ff92b4cef
mywaystay <mywaystay@126.com>
2020-06-23 11:48:11 +0800

    [修改] 修改鉴权方式

--- fileserver.go
+++ fileserver.go
@@ -977,44 +977,47 @@ func (this *Server) SetDownloadHeader(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Disposition", "attachment")
 }
 func (this *Server) CheckAuth(w http.ResponseWriter, r *http.Request) bool {
-	var (
-		err        error
-		req        *httplib.BeegoHTTPRequest
-		result     string
-		jsonResult JsonResult
-	)
-	if err = r.ParseForm(); err != nil {
-		log.Error(err)
-		return false
-	}
-	req = httplib.Post(Config().AuthUrl)
-	req.SetTimeout(time.Second*10, time.Second*10)
-	req.Param("__path__", r.URL.Path)
-	req.Param("__query__", r.URL.RawQuery)
-	for k, _ := range r.Form {
-		req.Param(k, r.FormValue(k))
-	}
-	for k, v := range r.Header {
-		req.Header(k, v[0])
-	}
-	result, err = req.String()
-	result = strings.TrimSpace(result)
-	if strings.HasPrefix(result, "{") && strings.HasSuffix(result, "}") {
-		if err = json.Unmarshal([]byte(result), &jsonResult); err != nil {
-			log.Error(err)
-			return false
-		}
-		if jsonResult.Data != "ok" {
-			log.Warn(result)
-			return false
-		}
-	} else {
-		if result != "ok" {
-			log.Warn(result)
-			return false
-		}
-	}
-	return true
+	//var (
+	//	err        error
+	//	req        *httplib.BeegoHTTPRequest
+	//	result     string
+	//	jsonResult JsonResult
+	//)
+	//if err = r.ParseForm(); err != nil {
+	//	log.Error(err)
+	//	return false
+	//}
+	//req = httplib.Post(Config().AuthUrl)
+	//req.SetTimeout(time.Second*10, time.Second*10)
+	//req.Param("__path__", r.URL.Path)
+	//req.Param("__query__", r.URL.RawQuery)
+	//for k, _ := range r.Form {
+	//	req.Param(k, r.FormValue(k))
+	//}
+	//for k, v := range r.Header {
+	//	req.Header(k, v[0])
+	//}
+	//result, err = req.String()
+	//result = strings.TrimSpace(result)
+	//if strings.HasPrefix(result, "{") && strings.HasSuffix(result, "}") {
+	//	if err = json.Unmarshal([]byte(result), &jsonResult); err != nil {
+	//		log.Error(err)
+	//		return false
+	//	}
+	//	if jsonResult.Data != "ok" {
+	//		log.Warn(result)
+	//		return false
+	//	}
+	//} else {
+	//	if result != "ok" {
+	//		log.Warn(result)
+	//		return false
+	//	}
+	//}
+	//return true
+
+	result, _ := this.CheckAuthWithInfo(w, r)
+	return result
 }
 
 //鉴权并返回响应信息
